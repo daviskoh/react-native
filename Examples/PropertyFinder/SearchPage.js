df50e783d3430f5578de8a38fce18d7414678f13
Davis Koh <koh.davis.0@gmail.com>
2015-03-27 15:25:11 -0400

    add search

--- Examples/PropertyFinder/SearchPage.js
+++ Examples/PropertyFinder/SearchPage.js
@@ -66,16 +66,47 @@ var styles = StyleSheet.create({
     }
 });
 
-function constructButton(buttonText) {
+function constructButton(buttonText, onPress) {
     return <TouchableHighlight style={styles.button}
-               underlayColor='#99d9f4'>
+               underlayColor='#99d9f4'
+               onPress={onPress}>
                <Text style={styles.buttonText}>{buttonText}</Text>
            </TouchableHighlight>   
 }
 
+function urlForQueryAndPage(key, value, pageNumber) {
+    var data = {
+        country: 'uk',
+        pretty: '1',
+        encoding: 'json',
+        listing_type: 'buy',
+        action: 'search_listings',
+        page: pageNumber
+    };
+
+    data[key] = value;
+
+    var querystring = Object.keys(data)
+                        .map(key => key + '=' + encodeURIComponent(data[key]))
+                        .join('&');
+
+    return 'http://api.nestoria.co.uk/api?' + querystring;
+}
+
 // TODO: consider drying factorying below button
 class SearchPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchString: 'lond',
+            isLoading: false,
+            message: ''
+        };
+    }
+
     render() {
+        var spinner = this.state.isLoading ? ( <ActivityIndicatorIOS hidden='true' size='large'/> ) : ( <View/> );
+
         return (
             <View style={styles.container}>
                 <Text style={styles.description}>
@@ -88,14 +119,53 @@ class SearchPage extends Component {
                 <View style={styles.flowRight}>
                     <TextInput
                         style={styles.searchInput}
+                        value={this.state.searchString}
+                        onChange={this.onSearchTextChanged.bind(this)}
                         placeholder='Search via name or postcode'/>
-                    {constructButton('Go')}
+                    {constructButton('Go', this.onSearchPressed.bind(this))}
                 </View>
 
                 {constructButton('Location')}
                 <Image source={require('image!house')} style={styles.image}/>
+                {spinner}
+
+                <Text style={styles.description}>{this.state.message}</Text>
             </View>
         );
+    }
+
+    onSearchTextChanged(event) {
+        console.log('onSearchTextChanged');
+        this.setState({ searchString: event.nativeEvent.text });
+        console.log(this.state.searchString);
+    }
+
+    _handleResponse(response) {
+        this.setState({ isLoading: false, message: '' });
+        if (response.application_response_code.substr(0, 1) === '1') {
+            console.log('Properties found: ' + response.listings.length);
+        } else {
+            this.setState({ message: 'Location not recognized; please try again.'});
+        }
+    }
+
+    _executeQuery(query) {
+        console.log(query);
+        this.setState({ isLoading: true });
+
+        fetch(query)
+            .then(response => response.json())
+            .then(json => this._handleResponse(json.response))
+            .catch(error =>
+                this.setState({
+                    isLoading: false,
+                    message: 'Something bad happened ' + error
+            }));
+    }
+
+    onSearchPressed() {
+        var query = urlForQueryAndPage('place_name', this.state.searchString, 1);
+        this._executeQuery(query);
     }
 }
 
