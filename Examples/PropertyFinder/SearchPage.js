90bd8d367a10484585b3a2f681653df25d9adb07
Davis Koh <koh.davis.0@gmail.com>
2015-03-26 20:47:56 -0400

    tutorial init

---
+++ Examples/PropertyFinder/SearchPage.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var React = require('react-native');
+
+// destructuring assignment
+// - instead of React.StyleSheet, use StyleSheet property directly
+var {
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    TouchableHighlight,
+    ActivityIndicatorIOS,
+    Image,
+    Component
+} = React;
+
+var styles = StyleSheet.create({
+    description: {
+        marginBottom: 20,
+        fontSize: 18,
+        textAlign: 'center',
+        color: '#656565'
+    },
+    container: {
+        padding: 30,
+        marginTop: 65,
+        alignItems: 'center'
+    }
+});
+
+class SearchPage extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.description}>
+                    Search for houses to buy!
+                </Text>
+                <Text style={styles.description}>
+                    Search by place-name, postcode or search near your location.
+                </Text>
+            </View>
+        );
+    }
+}
+
+module.exports = SearchPage;
+
